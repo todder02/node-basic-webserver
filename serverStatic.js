7e0350733d09b8affc87a5911ffc0b413ee3148d
Todd Mantell <Todd Mantell>
2016-01-03 09:00:39 -0600

    Initial Commit

---
+++ serverStatic.js
@@ -0,0 +1,49 @@
+(function() {    
+    'use strict';
+
+var staticModule = require('node-static');
+
+var file = new staticModule.Server(__dirname);
+
+var http = require('http');
+
+var server = http.createServer(createHTTP);
+
+var port = 80;
+
+server.listen(port);
+
+console.log("Listening on port " + port);
+
+function createHTTP(request, response) {
+    if (request.method === 'GET') {
+        request.addListener('end', () => {
+            file.serve(request, response, (e, res) => {
+            if (e && (e.status === 404)) { // If the file wasn't found 
+                //fileServer.serveFile('/not-found.html', 404, {}, request, response);
+                response.writeHead(404);
+                response.end("Oh no! The page you requested could not be found.");
+                 }
+            });
+        }).resume();
+        /*if (request.url === '/') {
+            request.addListener('end', function () {
+                file.serve(request, response);                
+            }).resume();
+        }
+        else if (request.url === '/emmetTest.html'){
+            request.addListener('end', function () {
+                file.serve(request, response);                
+            }).resume();
+        }
+        else {
+        response.writeHead(404);
+        response.end("Page Not Found");
+        }*/
+    }
+    else {
+        response.writeHead(404);
+        response.end("Page Not Found");
+    }
+};
+}());
